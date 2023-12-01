16faa9add2fbd69072bb347cdccce516110a4b7b
Levin Gatchalian <lgatchal@asu.edu>
2023-12-01 14:04:20 -0800

    big update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,82 @@
 // Levin Gatchailan
+
+"use strict";
+
+
+// LIGHT/DARK
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// __GUESSING GAME
+ 
+function checkGuess() {
+    const userGuess = parseInt(document.getElementById("userGuess").value);
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+  
+    const resultText = document.getElementById("result");
+    
+    if (userGuess === randomNumber) {
+        resultText.textContent = `Wahoo! ${randomNumber} is correct! You're a Super Star!`;
+    }else{
+        resultText.textContent = `Mama Mia! ${userGuess} is incorrect. The number was ${randomNumber}.`;
+    }
+}
+
+
+
+
+
+// __PRODUCT DISPLAY
+
+  
+
+
+// __FORM VALIDATION
+
+
+
+
+
+
+
+
+
+
+
+// 
+// 
+// 
+// 
+// ___________________EVENT HANDLERS_____________________
+
+
+
+
+// Game
+document.addEventListener('DOMContentLoaded', function() {
+    const submitButton = document.getElementById("submitButton");
+    submitButton.addEventListener('click', checkGuess);
+});
+
+
+// Form Validation
+document.getElementById("formSubmit").addEventListener("click", function(e){
+    checkValidate();
+    e.preventDefault();
+});
